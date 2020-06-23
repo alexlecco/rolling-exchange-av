8f23d11805d2b0f90c26c1463c03f2f77f56925d
alexlecco <alexlecco24@gmail.com>
2020-06-23 00:51:53 -0300

    import and use color constants for style

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './src/constants/colors'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Rolling Exchange</Text>
     </View>
   );
 }
@@ -12,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.background,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: colors.text
+  }
 });
---
+++ src/constants/colors.js
@@ -0,0 +1,7 @@
+export default colors = {
+  background: '#313C59',
+  primary: '#7CA290',
+  secondary: '#4C508D',
+  links: '#B810DA',
+  text: '#FFFFFF'
+}
