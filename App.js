6f675ed6770f867a922c7b600a6e557af1249306
alexlecco <alexlecco24@gmail.com>
2020-07-04 17:27:14 -0300

    add Statusbar to our app

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+import {
+  Provider as PaperProvider,
+  Button,
+} from 'react-native-paper'
 import colors from './src/constants/colors'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Rolling Exchange</Text>
-    </View>
+    <PaperProvider>
+      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+      {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <Text style={styles.text}>Rolling Exchange</Text>
+        </View>
+        <Button icon="camera" mode="contained" onPress={() => console.log('Presionado')}>
+          Presioná
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
@@ -18,6 +32,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: colors.text
-  }
+    color: colors.text,
+  },
+  card: {
+    backgroundColor: colors.primary,
+    width: '90%',
+    padding: 10,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  statusBarUnderlay: {
+    height: 28,
+    backgroundColor: colors.background,
+  },
 });
